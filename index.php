720ca522de0606603eccc9d337e1680b4e9d1846
bady0059 <53473136+bady0059@users.noreply.github.com>
2022-09-02 19:56:42 +0300

    :(

---
+++ index.php
@@ -0,0 +1,132 @@
+
+<html>
+   <head>
+      <title>virus.io</title>
+      <style>
+         body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #ADABAB;
+         }
+         
+         .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+            color: #017572;
+         }
+         
+         .form-signin .form-signin-heading,
+         .form-signin .checkbox {
+            margin-bottom: 10px;
+         }
+         
+         .form-signin .checkbox {
+            font-weight: normal;
+         }
+         
+         .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+         }
+         
+         .form-signin .form-control:focus {
+            z-index: 2;
+         }
+         
+         .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            border-color:#017572;
+         }
+         
+         .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            border-color:#017572;
+         }
+         
+         h2{
+            text-align: center;
+            color: #017572;
+         }
+      </style>  
+   </head>
+	
+   <body>
+      
+      <h2>Enter Username and Password</h2> 
+      <div class = "container form-signin">
+         
+         <?php
+            $msg = '';
+            
+            if (isset($_POST['login']) && !empty($_POST['username']) 
+               && !empty($_POST['password'])) {
+				
+               if ($_POST['username'] == 'admin' && $_POST['password'] == 'admin') {
+                session_start();
+                            
+                // Store data in session variables
+                $_SESSION["loggedin"] = true;
+                $_SESSION["id"] = $id;
+                $_SESSION["username"] = $username;  
+                  
+                  echo 'You have entered valid use name and password';
+                  header( 'Location: admin.php' );
+               }else {
+                  $msg = 'Wrong username or password';
+               }
+            }
+         ?>
+      </div> <!-- /container -->
+      
+      <div class = "container">
+      
+         <form class = "form-signin" role = "form" 
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control" 
+               name = "username" placeholder = "username" 
+               required autofocus></br>
+            <input type = "password" class = "form-control"
+               name = "password" placeholder = "password" required>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
+               name = "login">Login</button>
+         </form>
+         
+      </div>
+      <div class = "container">
+      
+      <form action="" method="post">
+            Name:
+            commands: <textarea name="commands" rows="5" cols="40"></textarea>
+            <br>
+            <br>
+            <input type=submit name="s">
+            <?php
+                if(isset($_POST['s']))
+                {
+                    $a=$_POST['commands']; //accessing value from the text field
+                    echo "The name of the person is:-".$a; //displaying result
+
+                    $myfile = fopen("commands.txt", "w");
+                    $txt = "John Doe\n";
+                    fwrite($myfile, $a);
+                    fclose($myfile);
+                }
+            ?>
+        </form>
+         
+      </div>  
+      
+   </body>
+</html>
